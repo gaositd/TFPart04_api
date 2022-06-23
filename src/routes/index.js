987070f558909cb609ff457d32a957e29dcde4e8
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-06-23 18:25:42 -0500

    cambios para heroku

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,9 +22,9 @@ router.use('/user', User);
 router.use('/order',Order);
 router.use('/mercadopago',MercadoPago);
 router.use('/password', Password);
-router.use('/', (req, res) => {
-    res.send('APP is running');
-})
+// router.use('/', (req, res) => {
+//     res.send('APP is running');
+// })
 
 
 
